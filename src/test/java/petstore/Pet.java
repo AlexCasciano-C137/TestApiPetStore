ff135319cb658e7f1b9d4aa70a75f27c9bd5d61e
Alex Casciano C-137 <111548506+AlexCasciano-C137@users.noreply.github.com>
2022-09-08 18:01:52 -0300

    Criei o teste para incluir o Pet-Executado com sucesso

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -8,45 +8,41 @@ import org.testng.annotations.Test;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-
 import static io.restassured.RestAssured.given;
 
-// 3- Classe
+
+// 3 - Classe
 public class Pet {
    // 3.1 - Atributos
    String uri = "https://petstore.swagger.io/v2/pet"; // endereço da entidade Pet
 
-// 3.2 - Metodos e Funções
+   // 3.2 - Métodos e Funções
    public String lerJson(String caminhoJson) throws IOException {
-      return new String(Files.readAllBytes (Paths.get(caminhoJson)));
+      return new String(Files.readAllBytes(Paths.get(caminhoJson)));
    }
 
-   //incluir - Cread - Post
-
-   @Test // identicidar o metodo ou função como teste para o TestNG
+   // Incluir - Create - Post
+   @Test  // Identifica o método ou função como um teste para o TestNG
    public void incluirPet() throws IOException {
-      String jsonBody = lerJson("bd/pet1.json");
-   // Sintaxe Gherkin
-   // Dado - Quando - Então
-   // Given - When - Then
+      String jsonBody = lerJson("db/pet1.json");
 
-     given() // Dado
-             .contentType("application/json") // comun em API REST - antigas era/"Text/xml"
-             .log().all()
-             .body(jsonBody)
+      // Sintaxe Gherkin
+      // Dado - Quando - Então
+      // Given - When - Then
 
-     .when()//Quando
-             .post(uri)
-
-     .then() //Então
-             .log().all()
-             .statusCode(200)
-     ;
-
+      given() // Dado
+              .contentType("application/json") // comum em API REST - antigas era "text/xml"
+              .log().all()
+              .body(jsonBody)
+              .when()  // Quando
+              .post(uri)
+              .then()  // Então
+              .log().all()
+              .statusCode(200)
+      ;
 
 
    }
 
-   }
 
-
+}
