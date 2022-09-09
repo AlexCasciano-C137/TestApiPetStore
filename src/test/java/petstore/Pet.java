f3a362bf875208d4eac677e773f70f400b3c75a7
Alex Casciano C-137 <111548506+AlexCasciano-C137@users.noreply.github.com>
2022-09-09 17:10:42 -0300

    Finalizado o Script -excluirPet = "delete"

--- src/test/java/petstore/Pet.java
+++ src/test/java/petstore/Pet.java
@@ -95,6 +95,27 @@ public class Pet {
 
       ;
 
+    }
+    @Test(priority = 4)
+
+    public void excluirPet(){
+       String petId = "1991051831";
+
+       given()
+               .contentType("application/json")
+               .log().all()
+       .when()
+               .delete(uri + "/" + petId )
+
+       .then()
+               .log().all()
+               .statusCode(200)
+               .body("code", is (200))
+               .body("type", is("unknown"))
+               .body("message", is(petId))
+       ;
+
+
 
     }
 }
